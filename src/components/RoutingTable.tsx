81af951bd2e99aa72b5ed6e374c929c34a502d4d
Samuel Lim <samlimyisheng@gmail.com>
2024-11-28 14:45:34 +0800

    rewrite

---
+++ src/components/RoutingTable.tsx
@@ -0,0 +1,21 @@
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHeader,
+} from "./ui/table";
+
+
+export default function RoutingTable() {
+    return(
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead></TableHead>
+                </TableRow>
+            </TableHeader>
+        </Table>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
@@ -58,4 +59,4 @@ module.exports = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
